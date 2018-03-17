4c8fe42c2da33d4460809806d32d54bdab5519ac
daniel <dansim76@gmail.com>
2018-03-17 12:59:19 -0500

    new

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var burger = {
+var Burger = {
   selectAll: function(cb) {
     orm.selectAll("burger", function(res) {
       cb(res);
@@ -22,4 +22,4 @@ var burger = {
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = burger;
+module.exports = Burger;
